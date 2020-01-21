1a28bc6a61bd27891daadadbcc9c5e065eef8957
scarlett <nickodelacruz96@gmail.com>
2020-01-21 02:54:25 -0800

    added my first function ask chase why the alert pops up before html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
 var x = 10;
-var gg = 50;
+var g = 50;
+var chris = "ceo";
+var chase ="10x";
 
-if (x == 10 && gg < 20 ){
-    console.log("Conditions met son");
-} else if (x > 50) {
-    console.log("else if");
-} else {
-    console.log("dead end");
+
+// if (x == 10 && g < 20 ){
+//     console.log("Conditions met son");
+// } else if (x > 50) {
+//     console.log("else if");
+// } else {
+//     console.log("dead end");
+// }
+
+function test(chris, chase) {
+    return chris+chase;
 }
+
+var sum = test(1, 2);
+alert(sum);
+
+// var array = [];
+
+// if (g == 50){
+ 
+// }
+
+// function 
+
+// function loop(x){
+//     if (x == 10)
+//     return loop;
+//     console.log(hello)
+
+// }
