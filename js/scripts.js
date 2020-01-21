115e2efbc506de4b3fb477f68011a5c57e3fcdbf
scarlett <nickodelacruz96@gmail.com>
2020-01-20 23:52:11 -0800

    ask chase how to write a function where everytime you click button it adds 1 into  variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-var x = 20;
-var y = 50;
+var x = 10;
+var gg = 50;
 
-if (x != 10){
-    console.log("If");
+if (x == 10 && gg < 20 ){
+    console.log("Conditions met son");
+} else if (x > 50) {
+    console.log("else if");
+} else {
+    console.log("dead end");
 }
